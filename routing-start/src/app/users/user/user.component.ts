e9274c5b1045230625efc1d82898699bd55019d7
AntonioE <antonio.esposito4@live.it>
2020-12-24 23:06:23 +0100

    routes dinamiche

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.user = { id: params["id"], name: params["name"] };
+    });
   }
 
+  onClick() {
+    this.router.navigate(["/users", "1", "Max"]);
+  }
 }
