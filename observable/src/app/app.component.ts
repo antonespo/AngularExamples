2885f435310d927adff214c3fa14af23b3cbf5d0
AntonioE <antonio.esposito4@live.it>
2021-01-02 18:04:08 +0100

    Observables examples (next, complete, error)

--- observable/src/app/app.component.ts
+++ observable/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { OnDestroy } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "./user.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(
+      (value: boolean) => {
+        this.userActivated = value;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
 }
--- observable/src/app/home/home.component.ts
+++ observable/src/app/home/home.component.ts
@@ -1,15 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ChildActivationStart } from "@angular/router";
+import { interval, Observable, Subscription } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor() {}
+  private counter: Subscription;
 
   ngOnInit() {
+    // this.counter = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    // NEXT
+    // const customCounter = new Observable((observer) => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     observer.next(count);
+    //     count++;
+    //   }, 1000);
+    // });
+
+    // ERROR
+    // const customCounter = new Observable((observer) => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     if (count > 7) {
+    //       observer.error(new Error("Counter exceded the range"));
+    //     }
+    //     observer.next(count);
+    //     count++;
+    //   }, 1000);
+    // });
+
+    // COMPLETE
+    const customCounter = new Observable<number>((observer) => {
+      let count = 0;
+      setInterval(() => {
+        if (count > 7) {
+          observer.complete();
+        }
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    // SUBSCRIPTION
+    // this.counter = customCounter.subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //   },
+    //   (error) => {
+    //     console.log(error.message);
+    //   },
+    //   () => {
+    //     console.log("3 is reached. Count completed!");
+    //   }
+    // );
+
+    // OPERATORS
+    // NB IN PIPE posso mettere infiniti operator di rxjs, non solo 1
+    // https://www.learnrxjs.io/learn-rxjs/operators
+    const customCounterPipe = customCounter.pipe(
+      filter((data: number, i: number) => {
+        if (data % 2 == 0) {
+          return true;
+        }
+      }),
+      map((data: number) => {
+        return `Round: ${data}`;
+      })
+    );
+
+    // SUBSCRIPTION
+    this.counter = customCounterPipe.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error.message);
+      },
+      () => {
+        console.log("7 is reached. Count completed!");
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.counter.unsubscribe();
+  }
 }
